fa46c661c872e7f7f098cd7decf09ba3162c1c14
Franz Reitner <franz.reitner@pco.de>
2022-02-03 16:46:20 +0100

    testit
    - test2.cpp added;

---
+++ test2.cpp
@@ -0,0 +1,40 @@
+#include <string>
+
+int main(int argc, char** argv)
+{
+  test3(blah, blah);
+}
+
+int dada()
+{
+	test();
+	test2();
+}
+
+
+void dort(int i)
+{
+  printf("da");
+  test1();
+}
+
+
+void dort2(int i)
+{
+  printf("da");
+  test1();
+}
+
+int dada2()
+{
+	test1(blah);
+	test2();
+	test3();
+}
+
+
+void dort3(int i)
+{
+  printf("da");
+  test1();
+}
